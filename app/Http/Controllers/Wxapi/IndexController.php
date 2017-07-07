8bd03e7e003c4ebe7898053c38b404f00af6df7c
hkl <hkl@S527919.domain>
2017-07-07 08:54:19 +0800

    NEW CHANGE

--- app/Http/Controllers/Wxapi/IndexController.php
+++ app/Http/Controllers/Wxapi/IndexController.php
@@ -10,8 +10,21 @@ class IndexController extends Controller
 {
     public function test() {
         $url = 'https://api.douban.com/v2/movie/top250?start=0';
-        $result = CommonUtils::test();
-        echo "<pre>";print_r($result);
+        //$url = 'http://www.douban.com/service/auth2/auth';
+        //$url = 'https://movie.douban.com/top250?start=0&filter=';
+        //$result = CommonUtils::test();
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 100);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $result = curl_exec($curl);
+        curl_close($curl);
+//        return $resData;
+
+        echo "<pre>";print_r(json_decode($result, true));
     }
 
 }
